40fb3a12f26556a961924893e7a05ca9f2ed1d37
Alvis <alvis@AlvisdeMacBook-Pro.local>
2023-10-22 14:37:37 +0800

    test quasar and icon

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="white" text-color="black" label="Standard" />
+      <q-btn color="primary" label="Primary" />
+      <q-btn color="secondary" label="Secondary" />
+      <q-btn color="amber" glossy label="Amber" />
+      <q-btn color="brown-5" label="Brown 5" />
+      <q-btn color="deep-orange" glossy label="Deep Orange" />
+      <q-btn color="purple" label="Purple" />
+      <q-btn color="black" label="Black" />
+    </div>
+
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="primary" icon="mail" label="On Left" />
+      <q-btn color="secondary" icon-right="mail" label="On Right" />
+      <q-btn
+        color="red"
+        icon="mail"
+        icon-right="send"
+        label="On Left and Right"
+      />
+      <br />
+      <q-btn icon="phone" label="Stacked" stack glossy color="purple" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup></script>
+
+<style scoped></style>
